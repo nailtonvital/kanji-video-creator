1be01d908f1dec45ead06adeb144bec556ce4dcb
nailtonvital <nailtonvital35@gmail.com>
2024-03-04 21:55:49 -0300

    feat: socket.io and ejs

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("fdzf");
+var express = require("express")
+var app = express()
+var http = require("http").createServer(app)
+var io = require("socket.io")(http)
+
+io.on("connection", socket => {
+    console.log("OK");
+    socket.on("disconnect", () => {
+        console.log("x disconnect");
+    });
+
+    socket.on("video", data => {
+        console.log(data.url);
+    })
+})
+
+app.use(express.static("public"))
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+http.listen(3000, (req, res) => {
+    console.log("Server is running on port 3000")
+})
